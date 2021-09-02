111c32e41b480ac1a674ce756873fd87e07e1f0b
Name <slavaold1@yandex.ru>
2021-09-02 23:05:17 +0400

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _10_module
+{
+    class task1
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Введите первое число ");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число");
+                int numberTwo = int.Parse(Console.ReadLine());
+
+
+                INewCalc newCalc = new Calc();
+                newCalc.Sum(number, numberTwo);
+
+            }
+            catch (FormatException)
+            {
+
+                Console.WriteLine("Некоректный ввод числа");
+            }
+
+
+
+
+        }
+        public interface INewCalc
+        {
+            void Sum(int a, int b);
+        }
+
+        public class Calc : INewCalc
+        {
+            void INewCalc.Sum(int a, int b)
+            {
+                Console.WriteLine(a + b);
+            }
+        }
+
+
+
+    }
+}
+
